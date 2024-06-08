e59872c6c30b08f83d5c3ea4338db8af77180fbc
Renee <renee.xu321@gmail.com>
2024-06-08 13:59:47 -0400

    added text displaying methods

--- src/Text.java
+++ src/Text.java
@@ -1,3 +1,25 @@
+import java.awt.*;
+
 public class Text {
     
+    Display display;
+    Graphics2D g2;
+    Font font;
+    
+    public Text(Display display) {
+        this.display = display;
+
+    }
+
+    public void displayText(Graphics2D g2, int x, int y, int size, String text) {
+        this.g2 = g2; 
+        font = new Font("Arial",Font.PLAIN,size);
+
+        for (String line : text.split("\n")) {
+            g2.drawString(line,x,y);
+            y += 30;
+        }
+    }
+
+
 }

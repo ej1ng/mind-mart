63678bd35ae21e43a52b0f3b5e9906991a981f6e
Emily Jing <emilyjing6@gmail.com>
2024-06-08 13:46:07 -0400

    added hints

---
+++ src/Conditions.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Conditions {
+   /**
+    * constructor: generates a random number
+    */
+    public Conditions() {
+        Random random = new Random();
+    }
+    
+}

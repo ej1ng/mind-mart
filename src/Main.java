9e53031fa617bc279e393924bdc470b36ad9c2c6
Emily Jing <emilyjing@v1042-wn-rt-a-52-201.campus-dynamic.uwaterloo.ca>
2024-06-08 10:21:24 -0400

    housekeeping

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,6 @@ import java.util.Random;
 class Main {
     public static void main (String[] args) {
         Story storyline = new Story(); // storyline object created (methods related to storyline, generating ascii art, etc.)
-        Random random = new Random(); // random object created to generate random elements
 
-        int index = random.nextInt(4) + 1; // index variable generates random number between 1-4
-        String graphic = storyline.art(index); // sets graphic variable to random ascii character using the art method
-
-        System.out.print(graphic); // displays out character generated
     }
 }
--- src/Story.java
+++ src/Story.java
@@ -2,36 +2,6 @@
  * story class that displays the graphics/storyline
  */
 public class Story {
-    private String graphic = ""; // sets a private string for the ascii art graphic
-    /**
-     * constructor
-     * pre: none
-     * post: storyline is run
-     */
-    public Story() { // autoruns stuff at the beginning (prints or sets variables of storyline)
-        // introductory scenes
-        System.out.println("Welcome to Mind-Mart!"); 
-    }
-    /**
-     * art method that returns a random ascii character
-     * pre: none
-     * post: graphic variable is returned
-     */
-    public String art(int index) {
-        switch (index) {
-            case 1:
-                graphic = String.format("%8s\n%8s\n%10s\n", "/\\___/\\", "( =^.^= )", "(\") (\")_/");
-                break;
-            case 2:
-                graphic = String.format("%7s\n%s\n%s\n","(\\___/)", "(' x ')", "c(\")(\")");
-                break;
-            case 3:
-                graphic = String.format("%6s\n%s\n%s\n", "^---^", "( `o´ )", "( u u )");
-                break;
-            case 4:
-                graphic = String.format("%7s\n%8s\n%s\n%6s\n", "_____", "(` v ´)", "((_____))", "^ ^");
-                break;
-        }
-        return graphic;
-    }
+
+    
 }

51e347bc1235de0cd21cbae4f1be3bb14991f8bd
Emily Jing <emilyjing6@gmail.com>
2024-06-08 11:54:56 -0400

    added condition descriptions and classes

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Random;
 class Main {
     public static void main (String[] args) {
         Story storyline = new Story(); // storyline object created (methods related to storyline, generating ascii art, etc.)
-
+        // create a conditions class
+        // create an items method
     }
 }

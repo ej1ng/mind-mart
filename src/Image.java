dc51bd26e8619c1db5b326211e631b6e233f5fde
Renee <renee.xu321@gmail.com>
2024-06-08 12:40:09 -0400

    added image display methods

--- src/Image.java
+++ src/Image.java
@@ -1,12 +1,25 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.*;
 
 public class Image {
 
-    public void getLocation() {
+    public BufferedImage getImage(String imagePath) {
+        BufferedImage image = null;
 
-        
+        try {
+            image = ImageIO.read(getClass().getResource(imagePath));
+            return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        return image;
+    }
+
+    public void displayImage(Graphics2D g2, BufferedImage image, int x, int y, int size) {
+        g2.drawImage(image, x, y, size, size, null);
     }
 
     
